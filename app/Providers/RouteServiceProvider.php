c9438d6b269bb0d650a65c8bc53bda8526c51d25
danielhr2001 <danial.hajirajabi.79@gmail.com>
2024-03-31 06:14:32 +0330

    new routing system

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -25,13 +25,15 @@ class RouteServiceProvider extends ServiceProvider
     public function boot(): void
     {
         RateLimiter::for('api', function (Request $request) {
-            return Limit::perMinute(60)->by($request->user()?->id ?: $request->ip());
+            return Limit::perMinute(40)->by($request->user()?->id ?: $request->ip());
         });
 
         $this->routes(function () {
-            Route::middleware('api')
-                ->prefix('api')
-                ->group(base_path('routes/api.php'));
+            foreach (glob(base_path('routes/api/v1/*.php')) as $file) {
+                Route::prefix('api/v1')
+                    ->middleware(['api'])
+                    ->group($file);
+            }
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
