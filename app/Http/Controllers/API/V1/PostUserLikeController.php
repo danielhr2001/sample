54e885e3fc05be43062ae1aba71ed35bc12274c7
danielhr2001 <danial.hajirajabi.79@gmail.com>
2024-03-31 06:14:08 +0330

    just policy, model, controller files that were empty

---
+++ app/Http/Controllers/API/V1/PostUserLikeController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PostUserLike;
+use App\Http\Requests\StorePostUserLikeRequest;
+use App\Http\Requests\UpdatePostUserLikeRequest;
+
+class PostUserLikeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostUserLikeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(PostUserLike $postUserLike)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatePostUserLikeRequest $request, PostUserLike $postUserLike)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(PostUserLike $postUserLike)
+    {
+        //
+    }
+}
