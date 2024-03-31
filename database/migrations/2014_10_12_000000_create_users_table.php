b717a5d53df3c113d78c02eae3470892a2fc6291
danielhr2001 <danial.hajirajabi.79@gmail.com>
2024-03-31 06:11:52 +0330

    defined migrations

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,9 +14,12 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->unsignedTinyInteger('is_ban')->default(0)->comment("0->is not ban,1->is ban");
+            $table->unsignedTinyInteger('is_admin')->default(0)->comment("0->user,1->admin");
+            $table->string('email')->nullable()->unique();
+            $table->string('phone_number')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
