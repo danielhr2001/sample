7d15c9547f077251fdeee4e06bb2a849e4549b3e
danielhr2001 <danial.hajirajabi.79@gmail.com>
2024-06-05 01:14:58 +0330

    fixed POST Model bug

--- app/Models/PostUserLike.php
+++ app/Models/PostUserLike.php
@@ -2,8 +2,10 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Post;
+use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class PostUserLike extends Model
 {
@@ -19,12 +21,12 @@ class PostUserLike extends Model
         'post_id',
     ];
 
-    public function users()
+    public function user()
     {
         return $this->belongsTo(User::class);
     }
-    
-    public function posts()
+
+    public function post()
     {
         return $this->belongsTo(Post::class);
     }
